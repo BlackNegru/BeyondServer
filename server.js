4fc2fb619fc414053b0015cdfbc475dcaa85d2ec
BlackNegru <iwannabeinyouforever@gmail.com>
2024-10-23 05:39:13 +0530

    Update server.js 3

--- server.js
+++ server.js
@@ -365,4 +365,4 @@ app.get('/users', async (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => console.log(Server running on http://0.0.0.0:${PORT}));
+app.listen(PORT, '0.0.0.0', () => console.log('Server running on http://0.0.0.0:${PORT}'));
